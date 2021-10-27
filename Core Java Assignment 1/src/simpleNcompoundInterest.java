1a8356975a77756e1ff46ec59914306b63521aeb
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-10-27 22:17:40 +0530

    simpleNcompoundInterest

---
+++ Core Java Assignment 1/src/simpleNcompoundInterest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class simpleNcompoundInterest
+{
+	public static void main(String args[ ])
+	{
+		double p, rate, t, sim,com;
+		System.out.println("Enter amount:");
+		Scanner obj=new Scanner(System. in);
+		p=obj.nextDouble();
+		System. out. println("Enter the No.of years:");
+		t=obj.nextDouble();
+		System.out. println("Enter the Rate of interest");
+		rate=obj.nextDouble();
+		sim=(p * t * rate)/100;
+		com=p * Math.pow(1.0+rate/100.0,t) - p;
+		System.out.println("Simple Interest="+sim );
+		System.out. println("Compound Interest="+com);
+	}
+}
