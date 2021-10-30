c82b82bc7cb9354b137e52cbb4947b27a011f3bc
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-10-30 15:16:52 +0530

    Question #7 All Classes

---
+++ OOPS Assignment/src/Question7/DessertItem.java
@@ -0,0 +1,30 @@
+package Question7;
+
+
+public abstract class DessertItem {
+protected String name;
+
+public DessertItem()
+{
+name = "";
+}
+
+public DessertItem(String name1)
+{
+name = name1;
+}
+
+public String getName()
+{
+return name;
+}
+
+public void setName(String name1)
+{
+name = name1;
+}
+
+public abstract double getCost();
+}
+
+
