ba13f33b62a8facc065dd2aaa25bd453fef08792
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-11-01 21:22:18 +0530

    Question #2

---
+++ Generics Assignment/src/question2.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Double> map = new HashMap<>();
+		map.put(10, 72.48);
+		map.put(20, 63.57);
+		map.put(30, 54.66);
+		map.put(40, 45.75);
+		map.put(50, 36.84);
+		System.out.print(map);
+
+	}
+
+}
