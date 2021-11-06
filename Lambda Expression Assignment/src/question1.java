792b5bf6152ae3e3b0f416bc57958dd0685a84c8
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-11-06 11:15:11 +0530

    Question #1

--- Lambda Expression Assignment/src/question1.java
+++ Lambda Expression Assignment/src/question1.java
@@ -1,8 +1,18 @@
-
+interface operations{
+	int op(int x, int y);
+}
 public class question1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		operations ad = (a,b)->(a+b);
+		operations sb = (a,b) ->(a-b);
+		operations ml = (a,b) ->(a*b);
+		//operations dv = (a,b) ->(a/b);
+		
+		System.out.println("[100 and 50]\nAddition:"+ad.op(100,50)+"\nSubtraction:"+sb.op(100, 50)
+		+"\nMultiplication:"+ml.op(100, 50));
+	
 
 	}
 
